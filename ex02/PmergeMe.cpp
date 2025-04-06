275289632dfa97324d1e9814d6eaf3209adb2885
tashiget <tashiget@student.42tokyo.jp>
2025-04-06 18:05:24 +0900

    test

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,5 +1,8 @@
 #include "PmergeMe.hpp"
 
+int count = 0;
+int count_tmp = 0;
+
 PmergeMe::PmergeMe()
 {
 	vec_.clear();
@@ -27,115 +30,191 @@ PmergeMe::~PmergeMe()
 
 }
 
-void	PmergeMe::CalculateVector(long unsigned int num)
+std::vector<std::vector<int> >	PmergeMe::CalculateVector(std::vector<std::vector<int> >& origin)
 {
-	if (num > vec_.size())
+	if (origin.size() == 1)
 	{
-#if DEBUG
-		std::cout << "narabikae owari" << std::endl;
-#endif
-		return ;
+		return origin;
 	}
-	for (std::vector<int>::iterator n = vec_.begin() + 2 * num - 1; n < vec_.end(); n += 2 * num)
+	std::vector<std::vector<int> > arg;
+	for (size_t n = 0; n + 1 < origin.size(); n += 2)
 	{
-		if (*n < *(n - num))
-			std::swap_ranges(n - num + 1, n + 1, n - num - num + 1);
-	}
-#if DEBUG
-	std::cout<< "current sinsuu " << num << ": ";
-	for (long unsigned int n = 0; n < vec_.size(); n++)
-		std::cout << vec_[n] << ", ";
-	std::cout << std::endl;
-#endif
-
-	CalculateVector(num * 2);
-	int count = tmp;
-	for (std::vector<int>::iterator n = vec_.begin() + num - 1; n < vec_.end(); n += 2 * num)
-	{
-		std::vector<int>::iterator it = std::lower_bound(ans_vec_.begin(), std::min(ans_vec_.begin() + 2 * count, ans_vec_.end()), *n);
-		ans_vec_.insert(it, *n);
+		std::vector<int> tmp;
+		if (origin[n].back() < origin[n + 1].back())
+		{
+			tmp.insert(tmp.end(), origin[n].begin(), origin[n].end());
+			tmp.insert(tmp.end(), origin[n + 1].begin(), origin[n + 1].end());
+		}
+		else
+		{
+			tmp.insert(tmp.end(), origin[n + 1].begin(), origin[n + 1].end());
+			tmp.insert(tmp.end(), origin[n].begin(), origin[n].end());
+		}
 		count++;
-		#if DEBUG
-			std::cout<< "current answer box: ";
-			for (long unsigned int n = 0; n < ans_vec_.size(); n++)
-				std::cout << ans_vec_[n] << ", ";
-			std::cout << std::endl;
-		#endif
+		arg.push_back(tmp);
 	}
-	if ((vec_.size() / num) % 2)
-		tmp++;
+	count_tmp = count;
+	std::vector<int> tmp = origin.back();
+	std::vector<std::vector<int> > sorted = CalculateVector(arg);
+
+	std::vector<std::vector<int> > ans;
+	int flag = 1;
+	int start = 0;
+	int j = 1;
+	size_t end = 1;
+	while (flag)
+	{
+		if (end >= sorted.size())
+		{
+			end = sorted.size();
+			flag = false;
+		}
+		for (size_t p = start; p < end; p++)
+		{
+			std::vector<int> big(sorted[p].begin() + sorted[p].size() / 2, sorted[p].end());
+			ans.push_back(big);
+		}
+		int sub = 0;
+		if (flag == false && origin.size() % 2)
+		{
+			ans.insert(MyBound(ans.begin(), ans.end(), tmp.back()), tmp);
+			sub = 1;
+		}
+		std::vector<int> it_ve;
+		for (int p = end; p > start; p--)
+		{
+			size_t it_end = start + p + sub - 1;
+			for (std::vector<int>::iterator it = it_ve.begin(); it != it_ve.end(); it++)
+			{
+				if (sorted[p - 1][sorted[p - 1].size() - 1] >= *it)
+					it_end++;
+			}
+			std::vector<int> small(sorted[p - 1].begin(), sorted[p - 1].begin() + sorted[p - 1].size() / 2);
+			it_ve.push_back(sorted[p - 1][sorted[p - 1].size() / 2 - 1]);
+			std::vector<std::vector<int> >::iterator pos = MyBound(ans.begin(), ans.begin() + it_end, sorted[p - 1][sorted[p - 1].size() / 2 - 1]);
+			ans.insert(pos, small);
+		}
+		start = end;
+		end = start + Jacob(j);
+		j++;
+	}
+	count_tmp = count;
+	return (ans);
 }
 
-void	PmergeMe::CalculateDeque(long unsigned int num)
+std::deque<std::deque<int> >	PmergeMe::CalculateDeque(std::deque<std::deque<int> >& origin)
 {
-	if (num > deq_.size())
+	if (origin.size() == 1)
 	{
-#if DEBUG
-		std::cout << "narabikae owari" << std::endl;
-#endif
-		return ;
+		return origin;
 	}
-	for (std::deque<int>::iterator n = deq_.begin() + 2 * num - 1; n < deq_.end(); n += 2 * num)
+	std::deque<std::deque<int> > arg;
+	for (size_t n = 0; n + 1 < origin.size(); n += 2)
 	{
-		if (*n < *(n - num))
-			std::swap_ranges(n - num + 1, n + 1, n + 1 - num - num);
-	}
-
-#if DEBUG
-	std::cout<< "current sinsuu " << num << ": ";
-	for (long unsigned int n = 0; n < deq_.size(); n++)
-		std::cout << deq_[n] << ", ";
-	std::cout << std::endl;
-#endif
-
-	CalculateDeque(num * 2);
-	int count = tmp;
-	for (std::deque<int>::iterator n = deq_.begin() + num - 1; n < deq_.end(); n += 2 * num)
-	{
-		std::deque<int>::iterator it = std::lower_bound(ans_deq_.begin(), std::min(ans_deq_.begin() + 2 * count, ans_deq_.end()), *n);
-		ans_deq_.insert(it, *n);
+		std::deque<int> tmp;
+		if (origin[n].back() < origin[n + 1].back())
+		{
+			tmp.insert(tmp.end(), origin[n].begin(), origin[n].end());
+			tmp.insert(tmp.end(), origin[n + 1].begin(), origin[n + 1].end());
+		}
+		else
+		{
+			tmp.insert(tmp.end(), origin[n + 1].begin(), origin[n + 1].end());
+			tmp.insert(tmp.end(), origin[n].begin(), origin[n].end());
+		}
 		count++;
-		#if DEBUG
-			std::cout<< "current answer box: ";
-			for (long unsigned int n = 0; n < ans_deq_.size(); n++)
-				std::cout << ans_deq_[n] << ", ";
-			std::cout << std::endl;
-		#endif
+		arg.push_back(tmp);
 	}
-	if ((deq_.size() / num) % 2)
-		tmp++;
+	count_tmp = count;
+	std::deque<int> tmp = origin.back();
+	std::deque<std::deque<int> > sorted = CalculateDeque(arg);
+
+	std::deque<std::deque<int> > ans;
+	int flag = 1;
+	int start = 0;
+	int j = 1;
+	size_t end = 1;
+	while (flag)
+	{
+		if (end >= sorted.size())
+		{
+			end = sorted.size();
+			flag = false;
+		}
+		for (size_t p = start; p < end; p++)
+		{
+			std::deque<int> big(sorted[p].begin() + sorted[p].size() / 2, sorted[p].end());
+			ans.push_back(big);
+		}
+		int sub = 0;
+		if (flag == false && origin.size() % 2)
+		{
+			ans.insert(MyBound(ans.begin(), ans.end(), tmp.back()), tmp);
+			sub = 1;
+		}
+		std::deque<int> it_ve;
+		for (int p = end; p > start; p--)
+		{
+			size_t it_end = start + p + sub - 1;
+			for (std::deque<int>::iterator it = it_ve.begin(); it != it_ve.end(); it++)
+			{
+				if (sorted[p - 1][sorted[p - 1].size() - 1] >= *it)
+					it_end++;
+			}
+			std::deque<int> small(sorted[p - 1].begin(), sorted[p - 1].begin() + sorted[p - 1].size() / 2);
+			it_ve.push_back(sorted[p - 1][sorted[p - 1].size() / 2 - 1]);
+			std::deque<std::deque<int> >::iterator pos = MyBound(ans.begin(), ans.begin() + it_end, sorted[p - 1][sorted[p - 1].size() / 2 - 1]);
+			ans.insert(pos, small);
+		}
+		start = end;
+		end = start + Jacob(j);
+		j++;
+	}
+	count_tmp = count;
+	return (ans);
 }
 
 void	PmergeMe::SortVector()
 {
-	tmp = 0;
+	count = 0;
 	clock_t	start = clock();
-	CalculateVector(1);
+	ans_vec_ = CalculateVector(vec_);
 	clock_t	end = clock();
 
 	std::cout << "After:\t";
 	for (long unsigned int n = 0; n < ans_vec_.size(); n++)
-		std::cout << ans_vec_[n] << " ";
+		std::cout << ans_vec_[n][0] << " ";
 	std::cout << std::endl;
 	double elapsed = double(end - start) / CLOCKS_PER_SEC * 1000;
 	std::cout << "Time to process a range of " << std::setw(4) << std::setfill(' ') << ans_vec_.size() << " elements with std::vector : " << elapsed << " msec" << std::endl;
+	#if DEBUG
+	std::cout << count << std::endl;
+	#endif
 }
 
 void	PmergeMe::SortDeque()
 {
-	tmp = 0;
+	count = 0;
 	clock_t	start = clock();
-	CalculateDeque(1);
+	ans_deq_ = CalculateDeque(deq_);
 	clock_t	end = clock();
 
-#if 0
+#if DEBUG
 	std::cout << "After:\t";
-	for (long unsigned int n = 0; n < ans_vec_.size(); n++)
-		std::cout << ans_vec_[n] << " ";
+	for (long unsigned int n = 0; n < ans_deq_.size(); n++)
+		std::cout << ans_deq_[n][0] << " ";
 	std::cout << std::endl;
 #endif
 	double elapsed = double(end - start) / CLOCKS_PER_SEC * 1000;
 	std::cout << "Time to process a range of " << std::setw(4) << std::setfill(' ') << ans_deq_.size() << " elements with std::deque  : " << elapsed << " msec" << std::endl;
+	#if DEBUG
+	std::cout << count << std::endl;
+	#endif
+}
+
+int PmergeMe::Jacob(int n)
+{
+	return (static_cast<int>(pow(2, n) - pow((-1), n)) / 3 * 2);
 }
 
 void	PmergeMe::SortAll(int ac, char **av)
@@ -143,6 +222,7 @@ void	PmergeMe::SortAll(int ac, char **av)
 	long	tmp;
 	char	*endptr;
 
+	std::cout << "Before:\t";
 	for (int n = 1; n < ac; n++)
 	{
 		tmp = strtol(*(av + n), &endptr, 10);
@@ -150,12 +230,10 @@ void	PmergeMe::SortAll(int ac, char **av)
 			std::cerr << "Error" << std::endl;
 			return ;
 		}
-		vec_.push_back(tmp);
-		deq_.push_back(tmp);
+		std::cout << tmp << " ";
+		vec_.push_back(std::vector<int>(1, tmp));
+		deq_.push_back(std::deque<int>(1, tmp));
 	}
-	std::cout << "Before:\t";
-	for (long unsigned int n = 0; n < vec_.size(); n++)
-		std::cout << vec_[n] << " ";
 	std::cout << std::endl;
 	SortVector();
 	SortDeque();
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,23 +6,28 @@
 # include <limits>
 # include <ctime>
 # include <cstdlib>
+# include <cmath>
 # include <algorithm>
 # include <iomanip>
-# define DEBUG 0
+# define DEBUG 1
 
+extern int count;
 class PmergeMe
 {
 private:
-	std::vector<int>	vec_;
-	std::vector<int>	ans_vec_;
-	std::deque<int>		deq_;
-	std::deque<int>		ans_deq_;
+	std::vector<std::vector<int> >	vec_;
+	std::vector<std::vector<int> >	ans_vec_;
+	std::deque<std::deque<int> >	deq_;
+	std::deque<std::deque<int> >	ans_deq_;
 	int					tmp;
 
-	void	SortVector();
-	void	SortDeque();
-	void	CalculateVector(long unsigned int num);
-	void	CalculateDeque(long unsigned int num);
+	void				SortVector();
+	void				SortDeque();
+	int					Jacob(int n);
+	template<typename T>
+	T MyBound(T first, T last, int val);
+	std::vector<std::vector<int> >				CalculateVector(std::vector<std::vector<int> >& origin);
+	std::deque<std::deque<int> >				CalculateDeque(std::deque<std::deque<int> >& origin);
 
 public:
 	PmergeMe();
@@ -31,3 +36,18 @@ public:
 	~PmergeMe();
 	void	SortAll(int ac, char **av);
 };
+
+template<typename T>
+T PmergeMe::MyBound(T first, T last, int val)
+{
+while (first < last) {
+	T mid = first + (last - first) / 2;
+	if (mid->back() < val) {
+		first = mid + 1;
+	} else {
+		last = mid;
+	}
+	count++;
+}
+return first;
+}
